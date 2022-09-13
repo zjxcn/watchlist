4598bc9b85512ea667425128a96a664d4bbabaf7
zjx <zjxcn@126.com>
2022-09-13 20:02:58 +0800

    Add database support

--- app.py
+++ app.py
@@ -1,13 +1,40 @@
+import os
+import sys
+import click
+
 from flask import Flask, url_for, render_template
 from markupsafe import escape
+from flask_sqlalchemy import SQLAlchemy
 
+WIN = sys.platform.startswith('win')
+if WIN:  # 如果是 Windows 系统，使用三个斜线
+    prefix = 'sqlite:///'
+else:  # 否则使用四个斜线
+    prefix = 'sqlite:////'
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+# 在扩展类实例化前加载配置
+db = SQLAlchemy(app)   # 初始化SQLAlchemy扩展类，传入程序实例 app
+
+
+class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    name = db.Column(db.String(20))  # 名字
+
+
+class Movie(db.Model):  # 表名将会是 movie
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    title = db.Column(db.String(60))  # 电影标题
+    year = db.Column(db.String(4))  # 电影年份
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', name=name, movies=movies)
+    user = User.query.first()  # 读取用户记录
+    movies = Movie.query.all()  # 读取所有电影记录
+    return render_template('index.html', user=user.name, movies=movies)
 
 
 @app.route('/user/<name>')
@@ -17,7 +44,7 @@ def user_page(name):
 
 @app.route('/Geek.inc')
 def hello():
-    return '<h1>Hello,Geek.inc!</h1>'
+    return '<h1> sys.platform is : ' + sys.platform + '</h1>'
 
 
 @app.route('/test')
@@ -33,16 +60,49 @@ def test_url_for():
     return 'Test page'
 
 
-name = 'ZhangJunxiang'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
+# 创建数据命令
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
+
+    # 全局的两个变量移动到这个函数内
+    name = 'Grey Li'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+
+    click.echo('Done.')
+
+
+# name = 'ZhangJunxiang'
+# movies = [
+#     {'title': 'My Neighbor Totoro', 'year': '1988'},
+#     {'title': 'Dead Poets Society', 'year': '1989'},
+#     {'title': 'A Perfect World', 'year': '1993'},
+#     {'title': 'Leon', 'year': '1994'},
+#     {'title': 'Mahjong', 'year': '1996'},
+#     {'title': 'Swallowtail Butterfly', 'year': '1996'},
+#     {'title': 'King of Comedy', 'year': '1999'},
+#     {'title': 'Devils on the Doorstep', 'year': '1999'},
+#     {'title': 'WALL-E', 'year': '2008'},
+#     {'title': 'The Pork of Music', 'year': '2012'},
+# ]
